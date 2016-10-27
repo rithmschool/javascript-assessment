4016bd18d2c79df95beb76cd3d96e1ab66520bee
Greg Marsella <gkmarsella@gmail.com>
2016-10-27 15:53:24 -0700

    Skipped over to part 2 and finished "Book" section.

--- assessment.js
+++ assessment.js
@@ -64,8 +64,97 @@ multiplyEvenNumbers([2,3,4,5,6]);
 
 
 // ************************************************************************
-// 							capitalize											
+// 							capitalize										   ---------x		
 // ************************************************************************
+
+
+function capitalize(str){
+    var toUpper = str[0].toUpperCase();
+    var capitalizedString = "";
+    for(var i = 1; i < str.length; i++){
+        capitalized
+    }
+}
+capitalize("gregory");
+
+
+
+// ************************************************************************
+// 							compact											   ---------x		
+// ************************************************************************
+
+
+
+function compact(arr){
+	var truthy = [];
+	for(var i=0; i < arr.length; i++){
+		if(arr[i] === true){
+			arr[i].push(truthy);
+		}
+	}
+	return truthy;
+}
+
+
+// ************************************************************************
+// 							flip										      ---------x		
+// ************************************************************************
+
+
+
+
+
+
+// ************************************************************************
+// 							once										      ---------x		
+// ************************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ***********************************************************************************************
+// 										 PART TWO		
+// ***********************************************************************************************
+
+
+function Book(title, genre, pageCount, author, numChapters){
+    this.title = title;
+    this.genre = genre;
+    this.pageCount = pageCount;
+    this.author = author;
+    this.numChapters = numChapters;
+}
+
+var theDarkTower = new Book("The Gunslinger", "fantasy", "224", "Stephen King", "11");
+
+var theDarkTower2 = new Book("The Drawing of the Three", "fantasy", "400", "Stephen King", "26");
+
+var theDarkTower3 = new Book("The Waste Lands", "fantasy", "512", "Stephen King", "32");
+
+Book.prototype.toString = function(){
+	return this.title + (": ") + this.pageCount + (" pages");
+};
+
+Book.prototype.pagesPerChapter = function(){
+	return ("The average number of pages per chapter is " + Math.round(this.pageCount / this.numChapters) + " pages");
+};
+
+
+
+
+
+
 
 
 
