42740ed34998a9e9a5106c4cf8d170cb8528440c
Raymond Chow <ravingrabbit75@gmail.com>
2016-10-27 15:07:30 -0700

    added frequency function

--- assessment.js
+++ assessment.js
@@ -1,2 +1,13 @@
 // Write your solutions here:
 
+function frequency(array, primitive){
+	var counter=0;
+	array.forEach(function(arrayItem,idx){
+		if(arrayItem===primitive){
+			counter++;
+		}
+	});
+
+	return counter;
+}
+
